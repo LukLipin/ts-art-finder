db17816df85f560fd835aa0400e783e9543d2010
luklipin <l.k.lipinski@gmail.com>
2025-11-18 09:57:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       
+      <h1 className="card">Art Finder</h1>
+    </>  
+  )
+}
+
+export default App
